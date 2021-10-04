364ff52eed1bbe5fda76420223c2b60ffd072920
Yuichiro Komori <thekomori1113@gmail.com>
2021-10-05 00:58:55 +0900

    chore: add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  root: true,
+  plugins: [
+    "react-hooks", "react", "@typescript-eslint"
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  plugins: [
+    "@typescript-eslint",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  settings: {
+    "react": {
+      "version": "detect"
+    }
+  },
+  rules: {
+    "import/no-unresolved": "off",
+    "import/no-cycle": "error",
+    "import/no-default-export": "off",
+
+    "@typescript-eslint/array-type": ["warn", { default: "generic" }],
+    "@typescript-eslint/naming-convention": [
+      "warn",
+      { "selector": "default", "format": ["camelCase", "UPPER_CASE", "PascalCase"], "leadingUnderscore": "allow" },
+      { "selector": "typeLike", "format": ["PascalCase"], "leadingUnderscore": "allow" },
+    ],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+  },
+};
